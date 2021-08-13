4090ee187f4d8fc71411818d3e7c8047ba576d60
Nik Van Baak <nvanbaak@gmail.com>
2021-08-13 12:14:29 -0700

    rearranged file structure, renamed main program to match project folder name due to Arduino requirements

---
+++ prototype code/read-thermistor/read-thermistor.ino
@@ -0,0 +1,31 @@
+int pinID = A0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Read analog input
+  int varLog = getTemp(pinID);
+
+  delay(1000);
+}
+
+int getTemp(int pinID) {
+
+  // get input from specified pin
+  int thermRead = analogRead(pinID);
+
+  // normalize analog read as a proportion of 5V
+  double volts = (thermRead * 5) / 1024.0;
+  
+  // convert voltage to resistance; this equation is for a voltage divider setup with a 22-kΩ resistor on the 5V end
+  double resistance = (110 - (22 * volts)) / volts;
+
+  // convert resistance to temp; equation derived from a scatter plot of test data points
+  double temp = (-47.072 * log(resistance)) + 233.89;
+
+  Serial.println("thermRead = " + String(thermRead) + "; volts = " + String(volts) + "; resistance = " + String(resistance) + "; temp = " + String(temp));
+
+  return temp;
+}
