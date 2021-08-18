2b8a17aae4c2250b5437d2a01dddc341d13186dc
Nik Van Baak <nvanbaak@gmail.com>
2021-08-18 12:28:28 -0700

    added basic heater control logic

--- hot-tub-replacement.ino
+++ hot-tub-replacement.ino
@@ -1,12 +1,22 @@
-unsigned long tenSecondCheck = 0;
-const int therm1 = A0;
-const int therm2 = A1;
+// Arduino pin assignments
+const int therm1Pin = A0;
+const int therm2Pin = A1;
 const int motorPins[3] = {12, 11, 10};
 const int motorRelayPins[3] = {7, 6, 5};
+
+// Motor state tracking
 int motorInput[3] = {1, 1, 1};
 int motorState[3] = {0, 0, 0};
-String motorStateLabels[3] = {"off", "low", "high"};
 int inputDelay[3] = {0, 0, 0};
+String motorStateLabels[3] = {"off", "low", "high"};
+
+// heater / temperature variables
+int tempTarget = 90;
+bool heaterOn = false;
+bool heaterWasOn = false;
+
+// misc counters
+unsigned long tenSecondCheck = 0;
 
 
 void setup() {
@@ -16,14 +26,31 @@ void setup() {
     pinMode(motorPins[i], INPUT_PULLUP);  
     pinMode(motorRelayPins[i], OUTPUT);
   }
-
 }
 
 void loop() {
 
+  // Every ten seconds, adjust hot tub behavior
   if (tenSecondsElapsed()) {
-    float temp1 = getTemp(therm1);
-    float temp2 = getTemp(therm2);
+    float temp1 = getTemp(therm1Pin);
+    float temp2 = getTemp(therm2Pin);
+
+    // If heater is on, decide whether to turn it off
+    if (heaterOn) {
+      if (temp2 > 105) { // Don't heat if hot tub is too hot
+        heaterOn = false;
+      } else if (temp2 >= tempTarget) { // Don't heat if we've hit the target
+        heaterOn = false;        
+      }      
+    } else {
+      if (temp2 < tempTarget - 1) { // heat if we're below the target
+        heaterOn = true;
+      }      
+    }
+
+    // digitalWrite(heaterPin, something conditional)
+    heaterWasOn = heaterOn;
+
     displayState(temp1, temp2);
   }
 
@@ -33,16 +60,33 @@ void loop() {
       inputDelay[i]--;
       
     } else {
-      // read input and determine if state has changed since last cycle   
-      int motorButtonState = digitalRead(motorPins[i]);
-      if (motorInput[i] != motorButtonState) {
-        motorInput[i] = motorButtonState;
-
-        // if the button went from open to pressed, we toggle the motor
-        if (motorButtonState == 0) {
-          toggleMotor(i);
-          inputDelay[i] = 250;
-        }
+      // read input and determine if state has changed since last cycle  
+      // We're currently working with a one-motor scale model, so this switch statement is temporary
+      switch (i) {
+        case 0:
+          int motorButtonState = digitalRead(motorPins[i]);
+          if (motorInput[i] != motorButtonState) {
+            motorInput[i] = motorButtonState;
+    
+            // if the button went from open to pressed, we toggle the motor
+            if (motorButtonState == 0) {
+              toggleMotor(i);
+              inputDelay[i] = 250;
+            }
+          }
+          break;
+        case 1:
+          if (tempTarget < 105) {
+            tempTarget++;
+          }
+          Serial.println("Set desired temp to " + String(tempTarget));
+          break;
+        case 2:
+          if (tempTarget > 90) {
+            tempTarget--;
+          }
+          Serial.println("Set desired temp to " + String(tempTarget));            
+          break;
       }
     }
   }
