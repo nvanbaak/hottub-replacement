7a15d845bf472b55f7c8bead179bd450d0cb83eb
Nik Van Baak <nvanbaak@gmail.com>
2021-08-18 13:54:45 -0700

    fixed main loop only checking motor pushbutton and ignoring temp pushbuttons

--- hot-tub-replacement.ino
+++ hot-tub-replacement.ino
@@ -3,12 +3,12 @@ const int therm1Pin = A0;
 const int therm2Pin = A1;
 const int motorPins[3] = {12, 11, 10};
 const int motorRelayPins[3] = {7, 6, 5};
+const int heaterPin = 9;
 
 // Motor state tracking
 int motorInput[3] = {1, 1, 1};
 int motorState[3] = {0, 0, 0};
 int inputDelay[3] = {0, 0, 0};
-String motorStateLabels[3] = {"off", "low", "high"};
 
 // heater / temperature variables
 int tempTarget = 90;
@@ -20,12 +20,17 @@ unsigned long tenSecondCheck = 0;
 
 
 void setup() {
+  // begin serial output
   Serial.begin(9600);
 
+  // Setup pins
   for (int i; i < 3; i++) {
     pinMode(motorPins[i], INPUT_PULLUP);  
     pinMode(motorRelayPins[i], OUTPUT);
   }
+  pinMode(heaterPin, OUTPUT);
+
+  displayState(getTemp(therm1Pin), getTemp(therm2Pin));
 }
 
 void loop() {
@@ -35,21 +40,7 @@ void loop() {
     float temp1 = getTemp(therm1Pin);
     float temp2 = getTemp(therm2Pin);
 
-    // If heater is on, decide whether to turn it off
-    if (heaterOn) {
-      if (temp2 > 105) { // Don't heat if hot tub is too hot
-        heaterOn = false;
-      } else if (temp2 >= tempTarget) { // Don't heat if we've hit the target
-        heaterOn = false;        
-      }      
-    } else {
-      if (temp2 < tempTarget - 1) { // heat if we're below the target
-        heaterOn = true;
-      }      
-    }
-
-    // digitalWrite(heaterPin, something conditional)
-    heaterWasOn = heaterOn;
+    heaterLogic(temp1, temp2);
 
     displayState(temp1, temp2);
   }
@@ -61,38 +52,35 @@ void loop() {
       
     } else {
       // read input and determine if state has changed since last cycle  
-      // We're currently working with a one-motor scale model, so this switch statement is temporary
-      switch (i) {
-        case 0:
-          int motorButtonState = digitalRead(motorPins[i]);
-          if (motorInput[i] != motorButtonState) {
-            motorInput[i] = motorButtonState;
-    
-            // if the button went from open to pressed, we toggle the motor
-            if (motorButtonState == 0) {
-              toggleMotor(i);
-              inputDelay[i] = 250;
+      int motorButtonState = digitalRead(motorPins[i]);
+      if (motorInput[i] != motorButtonState) {
+        // We're currently working with a one-motor scale model, so this switch statement is temporary
+        switch (i) {
+          case 0:
+              motorInput[i] = motorButtonState;
+              // if the button went from open to pressed, we toggle the motor
+              if (motorButtonState == 0) {
+                toggleMotor(i);
+              }
+            break;
+          case 1:
+            if (tempTarget <= 105) {
+              tempTarget++;
             }
-          }
-          break;
-        case 1:
-          if (tempTarget < 105) {
-            tempTarget++;
-          }
-          Serial.println("Set desired temp to " + String(tempTarget));
-          break;
-        case 2:
-          if (tempTarget > 90) {
-            tempTarget--;
-          }
-          Serial.println("Set desired temp to " + String(tempTarget));            
-          break;
+            Serial.println("Set desired temp to " + String(tempTarget));
+            break;
+          case 2:
+            if (tempTarget > 90) {
+              tempTarget--;
+            }
+            Serial.println("Set desired temp to " + String(tempTarget));            
+            break;
+        }
+        inputDelay[i] = 250;
       }
     }
   }
-  
   delay(1); // otherwise the inputDelay statement goes by way too fast
-
 }
 
 float getTemp(int pinID) {
@@ -138,12 +126,42 @@ bool tenSecondsElapsed() {
   }
 }
 
+void heaterLogic(float temp1, float temp2) {
+  // If heater is on, decide whether to turn it off
+  if (heaterOn) {
+    if (temp2 > 105) { // Don't heat if hot tub is too hot
+      heaterOn = false;
+    } else if (temp2 >= tempTarget) { // Don't heat if we've hit the target
+      heaterOn = false;        
+    }      
+  } else {
+    if (temp2 < tempTarget - 1) { // heat if we're below the target
+      heaterOn = true;
+    }
+  }
+
+  // digitalWrite to heater if it needs to change
+  if (heaterOn != heaterWasOn) {
+    if (heaterOn) {
+      digitalWrite(heaterPin, HIGH);
+      Serial.println("Heater on.");
+    } else {
+      digitalWrite(heaterPin, LOW);
+      Serial.println("Heater off.");
+    }
+  }
+  // Then store this cycle's heater value for the next cycle to compare against
+  heaterWasOn = heaterOn;
+}
+
 void displayMotorState(int motorID) {
-  String stateName[] = {"off", "low", "high"};
-  Serial.println("Motor " + String(motorID) + " is " + stateName[motorState[motorID]] + ".");
+  String motorStateLabels[3] = {"off", "low", "high"};
+  Serial.println("Motor " + String(motorID) + " is " + motorStateLabels[motorState[motorID]] + ".");
 }
 
 void displayState(float temp1, float temp2) {
-//  Serial.println("Therm " + String(thermID) + ": thermRead = " + String(thermRead) + "; volts = " + String(volts) + "; resistance = " + String(resistance) + "; temp = " + String(temp));
-    Serial.println("Temp 1: " + String(temp1) + "; Temp 2: " + String(temp2) + "; Motors: " + "1:" + motorStateLabels[motorState[0]] + " 2:" + motorStateLabels[motorState[1]] + " 3:" + motorStateLabels[motorState[2]]);
+  String heaterState = heaterOn ? "on" : "off";
+  String motorStateLabels[3] = {"off", "low", "high"};
+
+  Serial.println("Temp1: " + String(temp1) + "; Temp2: " + String(temp2) + " Heater: " + heaterState + "; Motors: " + "1:" + motorStateLabels[motorState[0]] + " 2:" + motorStateLabels[motorState[1]] + " 3:" + motorStateLabels[motorState[2]]);
 }
