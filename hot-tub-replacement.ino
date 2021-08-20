d754bb760002ab021f24492d41f995c61e986c2b
Nik Van Baak <nvanbaak@gmail.com>
2021-08-20 12:07:14 -0700

    refactored temperature controls to match the original temperature control inputs

--- hot-tub-replacement.ino
+++ hot-tub-replacement.ino
@@ -12,13 +12,18 @@ int motorState[3] = {0, 0, 0};
 unsigned long inputLastTriggered[3] = {0, 0, 0};
 int motorInputDelayTime = 250; // in ms
 
-// temperature input variables
+// temperature input timing variables
 const int tempInputDelay = 250; // in ms
-const int tempInputTimeoutDelay = 750; // in ms
+const int tempInputReverseDelay = 750; // in ms
+const int tempInputTimeoutDelay = 1500; // in ms
 unsigned long tempInputLastTriggered = 0;
 
-// heater / temperature variables
+// temp input control variables
+bool tempIncreasesWhenPressed = true;
 int tempTarget = 90;
+int tempButtonPrevState;
+
+// heater / temperature variables
 float tempAvg1[2] = {0, 0};
 float rawTempData[2] = {0, 0};
 unsigned long lastTempCheck= 0;
@@ -52,23 +57,21 @@ void setup() {
 
 void loop() {
 
-  // temperature is taken multiple times per upkeep, then averaged
+  // temperature is taken multiple times per upkeep, then averaged at the upkeep
   if ( timeSince(lastTempCheck) >= tempSampleRate ) {
-
       rawTempData[0] += getTemp(therm1Pin);
       rawTempData[1] += getTemp(therm2Pin);
       tempSamples++;
-    
   }
 
-  // Every ten seconds, adjust hot tub behavior
+  // Upkeep method adjusts hot tub behavior based on various parameters
   if (timeSince(lastUpkeepCheck) >= upkeepInterval) {
     upkeep();
     lastUpkeepCheck = millis();
   }
 
+  // Check inputs, starting with zone jets
   for (int i = 0; i < 3; i++) {
-    
     // if we haven't triggered recently,
     if (timeSince(inputLastTriggered[i]) >= motorInputDelayTime) {
 
@@ -76,34 +79,41 @@ void loop() {
       int motorButtonState = digitalRead(motorPins[i]);
       if (motorInput[i] != motorButtonState) {
         
-        // We're currently working with a one-motor scale model, so this switch statement is temporary
-        switch (i) {
-          case 0:
-              motorInput[i] = motorButtonState;
-              // if the button went from open to pressed, we toggle the motor
-              if (motorButtonState == 0) {
-                toggleMotor(i);
-              }
-            break;
-          case 1:
-            if (tempTarget <= 105) {
-              tempTarget++;
-            }
-            Serial.println("Set desired temp to " + String(tempTarget));
-            break;
-          case 2:
-            if (tempTarget > 90) {
-              tempTarget--;
-            }
-            Serial.println("Set desired temp to " + String(tempTarget));            
-            break;
-        }
+        motorInput[i] = motorButtonState;
+        // if the button went from open to pressed, we toggle the motor
+        toggleMotor(i);
 
         // tell the program that we've triggered
         inputLastTriggered[i] = millis();
       }
     }
   }
+
+  // check temp input
+  long tempInputTime = timeSince(tempInputLastTriggered)
+  if (tempInputTime > tempInputDelay) {
+
+    // if debounce interval has passed, check if the input's changed
+    int tempButtonState = digitalRead(tempControlPin);
+    if (tempButtonState != tempButtonPrevState) {
+      
+      if (tempInputTime > tempInputTimeoutDelay) { // If we've timed out, we go to default settings
+        tempIncreasesWhenPressed = true;
+      } else if (tempInputTime > tempInputReverseDelay) { // if we've waited to press, invert temp control
+        tempIncreasesWhenPressed = !tempIncreasesWhenPressed;
+      }
+  
+      // Adjust temp according to temp control setting
+      tempTarget = tempIncreasesWhenPressed ? tempTarget++ : tempTarget--;
+      // tell the program we triggered at this timestamp
+      tempInputLastTriggered = millis();
+    }
+
+    tempButtonPrevState = tempButtonState;
+  }
+  
+
+  
 } // end of loop()
 
 void upkeep() {    
@@ -120,19 +130,14 @@ void upkeep() {
 }
 
 float getTemp(int pinID) {
-
   // get input from specified pin
   int thermRead = analogRead(pinID);
-
   // normalize analog read as a proportion of 5V
   float volts = (thermRead * 5) / 1024.0;
-  
   // convert voltage to resistance; this equation is for a voltage divider setup with a 22.1-kΩ resistor on the 5V end
   float resistance = 22.1 * (5 - volts) / volts;
-
   // convert resistance to temp; equation derived from a scatter plot of test data points
   float temp = (-47.072 * log(resistance)) + 233.89;
-
   return temp;
 }
 
